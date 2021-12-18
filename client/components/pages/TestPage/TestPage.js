ab93900a8ae9c333b25a2a8855e4fad008fa6302
Jihwan Kim <bluewhale.marc@gmail.com>
2021-12-18 19:00:46 +0900

    calc accuracy and speed

--- client/components/pages/TestPage/TestPage.js
+++ client/components/pages/TestPage/TestPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { push } from "connected-react-router";
 import { attempToPostTestResult } from "_thunks/tests";
@@ -11,17 +11,31 @@ export default function TestPage() {
   const [submittedText, setSubmittedText] = useState("");
   const [presentedText, setPresentedText] = useState("");
   const [progress, setProgress] = useState(0);
+  const [startTime, setStartTime] = useState(new Date());
+  const [accuracy, setAccuracy] = useState(0);
+  const [speed, setSpeed] = useState(0.0);
+  const [isStarted, setIsStarted] = useState(false);
+  const submitInput = useRef();
 
   const updateText = (e) => {
+    if (e.target.value.length == 1 && isStarted == false) {
+      setStartTime(new Date());
+      setIsStarted(true);
+    }
     setSubmittedText(e.target.value);
   };
 
   const onSubmitText = () => {
     setProgress(progress + 1);
-    dispatch(attempToPostTestResult(presentedText, submittedText, 0))
-      .catch(R.identity())
-      .then(() => {});
+    // let endTime = new Date();
+    // let leadTime = endTime.getTime() - startTime.getTime();
+    // dispatch(attempToPostTestResult(presentedText, submittedText, leadTime))
+    //   .catch(R.identity())
+    //   .then(() => {});
     setSubmittedText("");
+    setIsStarted(false);
+    setAccuracy(0);
+    submitInput.current.focus();
   };
 
   useEffect(() => {
@@ -30,6 +44,23 @@ export default function TestPage() {
     }
     setPresentedText(lists[progress]);
   }, [progress]);
+
+  useEffect(() => {
+    // Calc speed
+    if (submittedText.length > 0) {
+      let currentTime = new Date();
+      let leadSec = (currentTime.getTime() - startTime.getTime()) / 1000;
+      setSpeed(((submittedText.length / leadSec) * 60).toFixed(3));
+    }
+    // Calc accuracy
+    let errorTextCount = 0;
+    for (let i = 0; i < submittedText.length; i++) {
+      if (submittedText[i] != presentedText[i]) {
+        errorTextCount++;
+      }
+    }
+    setAccuracy((1 - errorTextCount / submittedText.length) * 100);
+  }, [submittedText]);
 
   return (
     <div className="row">
@@ -56,9 +87,16 @@ export default function TestPage() {
               className="validate"
               value={submittedText}
               onChange={updateText}
+              ref={submitInput}
             />
             <label htmlFor="submitter">Type here</label>
           </div>
+        </div>
+        <div className="col s12">
+          <h6 className="right">{`accuracy : ${accuracy} %`}</h6>
+        </div>
+        <div className="col s12">
+          <h6 className="right">{`speed : ${speed} / min`}</h6>
         </div>
         <div className="col s12">
           <div className="btn purple lighten-2" onClick={onSubmitText}>
