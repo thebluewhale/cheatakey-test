9094c80d62316de59c9976ddf99d388690f68ff2
Jihwan Kim <bluewhale.marc@gmail.com>
2022-01-03 16:30:08 +0900

    db account is changed

--- server/config/environment.js
+++ server/config/environment.js
@@ -1,5 +1,5 @@
 process.env.DATABASE_URL =
-  "mongodb+srv://marc:marc@cluster0.axx0v.mongodb.net/cheatakey";
+  "mongodb+srv://cheatakey:cheatakey@cluster0.jxdvm.mongodb.net/cheatakey?retryWrites=true&w=majority";
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 process.env.PORT = process.env.PORT || 3000;
 process.env.SESSION_SECRET = process.env.SESSION_SECRET || "cheatakey";
