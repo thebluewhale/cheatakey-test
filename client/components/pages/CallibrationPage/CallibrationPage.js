14cc277fe4d411639751d6203c4f6cab709a8251
Jihwan Kim <bluewhale.marc@gmail.com>
2022-03-11 18:55:42 +0900

    Apply callibration algorythm

--- client/components/pages/CallibrationPage/CallibrationPage.js
+++ client/components/pages/CallibrationPage/CallibrationPage.js
@@ -23,7 +23,26 @@ export default function CallibrationPage() {
   const [vowelKeyImageSrc, setVowelKeyImageSrc] = useState("vowel_key_type_2");
   const [isTerminated, setIsTerminated] = useState(false);
   const [chartData, setChartData] = useState([]);
+  const [callibrationData, setCallibrationData] = useState([
+    { min: 0, max: 0 }, // for a
+    { min: 0, max: 0 }, // for e
+    { min: 0, max: 0 }, // for i
+    { min: 0, max: 0 }, // for o
+    { min: 0, max: 0 }, // for u
+  ]);
   const MAX_PROGRESS = 15;
+  const ALPHABET_IDNEX_A = 0;
+  const ALPHABET_IDNEX_E = 1;
+  const ALPHABET_IDNEX_I = 2;
+  const ALPHABET_IDNEX_O = 3;
+  const ALPHABET_IDNEX_U = 4;
+  const GESTURE_DEGREE_DATA = [
+    { start: 240, end: 315 }, // for a
+    { start: 45, end: 120 }, // for e
+    { start: 315, end: 45 }, // for i
+    { start: 180, end: 240 }, // for o
+    { start: 120, end: 180 }, // for u
+  ];
 
   const shuffle = (array) => {
     for (let index = array.length - 1; index > 0; index--) {
@@ -71,7 +90,7 @@ export default function CallibrationPage() {
 
   const displayKey = (index) => {
     switch (index) {
-      case 0:
+      case ALPHABET_IDNEX_A:
         return (
           <div
             className="col s2 offset-s1 key key-normal"
@@ -79,7 +98,7 @@ export default function CallibrationPage() {
             onTouchEnd={touchEnd}
           ></div>
         );
-      case 1:
+      case ALPHABET_IDNEX_E:
         return (
           <div
             className="col s2 offset-s5 key key-normal"
@@ -87,7 +106,7 @@ export default function CallibrationPage() {
             onTouchEnd={touchEnd}
           ></div>
         );
-      case 2:
+      case ALPHABET_IDNEX_I:
         return (
           <div
             className="col s2 offset-s9 key key-normal"
@@ -99,32 +118,112 @@ export default function CallibrationPage() {
   };
 
   const PieChartGenerator = (data) => {
-    console.log("piechart generator");
+    callibrationData.map((item) => {
+      console.log(item.min, item.max);
+    });
     return (
       <ResponsivePie
         data={data}
-        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        margin={{ top: 10, right: 30, bottom: 10, left: 30 }}
         innerRadius={0.05}
-        padAngle={1}
+        padAngle={2}
         cornerRadius={3}
         activeOuterRadiusOffset={8}
-        colors={{ scheme: "category10" }}
+        colors={{ scheme: "pastel2" }}
         borderWidth={1}
         borderColor={{
           from: "color",
           modifiers: [["darker", 0.2]],
         }}
-        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsSkipAngle={0}
+        arcLinkLabelsDiagonalLength={10}
+        arcLinkLabelsStraightLength={10}
         arcLinkLabelsTextColor="#333333"
-        arcLinkLabelsThickness={2}
-        arcLinkLabelsColor={{ from: "color" }}
+        arcLinkLabelsThickness={3}
+        arcLinkLabelsColor={{ theme: "grid.line.stroke" }}
+        arcLinkLabel={(item) => `${item.label}`}
         arcLabelsSkipAngle={10}
         arcLabelsTextColor={{
           from: "color",
           modifiers: [["darker", 2]],
         }}
+        endAngle={360}
+        startAngle={-45 - callibrationData[ALPHABET_IDNEX_I].min}
+        arcLabel={(item) =>
+          `${
+            GESTURE_DEGREE_DATA[item.id].start - callibrationData[item.id].min
+          }° ~ ${
+            GESTURE_DEGREE_DATA[item.id].end - callibrationData[item.id].max
+          }°`
+        }
       />
     );
+  };
+
+  const processCallibrationData = (alphabetIndex, degree) => {
+    let cbd = callibrationData;
+    switch (alphabetIndex) {
+      case ALPHABET_IDNEX_A: // for a : 240 - 315
+        if (degree < 240 && degree > 195) {
+          console.log("min++");
+          cbd[ALPHABET_IDNEX_A].min++;
+          cbd[ALPHABET_IDNEX_O].max--;
+        } else if (degree > 315 && degree < 360) {
+          console.log("max++");
+          cbd[ALPHABET_IDNEX_A].max++;
+          cbd[ALPHABET_IDNEX_I].min--;
+        }
+        setCallibrationData(cbd);
+        return;
+      case ALPHABET_IDNEX_E: // for e : 45 - 120
+        if (degree < 45 && degree > 90) {
+          console.log("min++");
+          cbd[ALPHABET_IDNEX_E].min++;
+          cbd[ALPHABET_IDNEX_I].max--;
+        } else if (degree > 120 && degree < 165) {
+          console.log("max++");
+          cbd[ALPHABET_IDNEX_E].max++;
+          cbd[ALPHABET_IDNEX_U].min--;
+        }
+        setCallibrationData(cbd);
+        return;
+      case ALPHABET_IDNEX_I: //for i : 315-45
+        if (degree < 315 && degree > 270) {
+          console.log("min++");
+          cbd[ALPHABET_IDNEX_I].min++;
+          cbd[ALPHABET_IDNEX_A].max--;
+        } else if (degree > 45 && degree < 90) {
+          console.log("max++");
+          cbd[ALPHABET_IDNEX_I].max++;
+          cbd[ALPHABET_IDNEX_E].min--;
+        }
+        setCallibrationData(cbd);
+        return;
+      case ALPHABET_IDNEX_O: //for o : 180 - 240
+        if (degree < 180 && degree > 135) {
+          console.log("min++");
+          cbd[ALPHABET_IDNEX_O].min++;
+          cbd[ALPHABET_IDNEX_U].max--;
+        } else if (degree > 240 && degree < 285) {
+          console.log("max++");
+          cbd[ALPHABET_IDNEX_O].max++;
+          cbd[ALPHABET_IDNEX_A].min--;
+        }
+        setCallibrationData(cbd);
+        return;
+      case ALPHABET_IDNEX_U: //for u : 120 - 180
+        if (degree < 120 && degree > 75) {
+          console.log("min++");
+          cbd[ALPHABET_IDNEX_U].min++;
+          cbd[ALPHABET_IDNEX_E].max--;
+        } else if (degree > 180 && degree < 225) {
+          console.log("max++");
+          cbd[ALPHABET_IDNEX_U].max++;
+          cbd[ALPHABET_IDNEX_O].min--;
+        }
+        setCallibrationData(cbd);
+        return;
+    }
   };
 
   const touchStart = (e) => {
@@ -144,7 +243,16 @@ export default function CallibrationPage() {
     let submittedAngle =
       Math.atan2(clientY - touchStartY, clientX - touchStartX) *
       (180 / Math.PI);
+
+    // modify submittedAngle. 12 o'clock will be 0 deg, 3 o'clock will be 90 deg.
     if (submittedAngle < 0.0) submittedAngle += 360.0;
+    submittedAngle += 90;
+    submittedAngle %= 360;
+    processCallibrationData(
+      gestureDirectionArr[gestureDirectionIndex],
+      submittedAngle
+    );
+
     let tempArr = [];
     if (gestureDirectionIndex == 4) {
       if (testSetCount == 2 || testSetCount == 5) {
@@ -179,34 +287,50 @@ export default function CallibrationPage() {
       setIsTerminated(true);
       setChartData([
         {
-          id: "java",
-          label: "java",
-          value: 305,
-          color: "hsl(225, 70%, 50%)",
+          id: ALPHABET_IDNEX_I,
+          label: "i",
+          value:
+            90 +
+            callibrationData[ALPHABET_IDNEX_I].min +
+            callibrationData[ALPHABET_IDNEX_I].max,
+          color: "hsl(206, 70%, 50%)",
         },
         {
-          id: "scala",
-          label: "scala",
-          value: 436,
+          id: ALPHABET_IDNEX_E,
+          label: "e",
+          value:
+            75 +
+            callibrationData[ALPHABET_IDNEX_E].min +
+            callibrationData[ALPHABET_IDNEX_E].max,
           color: "hsl(168, 70%, 50%)",
         },
         {
-          id: "haskell",
-          label: "haskell",
-          value: 187,
-          color: "hsl(206, 70%, 50%)",
-        },
-        {
-          id: "stylus",
-          label: "stylus",
-          value: 539,
-          color: "hsl(206, 70%, 50%)",
-        },
-        {
-          id: "sass",
-          label: "sass",
-          value: 132,
+          id: ALPHABET_IDNEX_U,
+          label: "u",
+          value:
+            60 +
+            callibrationData[ALPHABET_IDNEX_U].min +
+            callibrationData[ALPHABET_IDNEX_U].max,
           color: "hsl(97, 70%, 50%)",
+        },
+
+        {
+          id: ALPHABET_IDNEX_O,
+          label: "o",
+          value:
+            60 +
+            callibrationData[ALPHABET_IDNEX_O].min +
+            callibrationData[ALPHABET_IDNEX_O].max,
+          color: "hsl(206, 70%, 50%)",
+        },
+        {
+          id: ALPHABET_IDNEX_A,
+          label: "a",
+          value:
+            75 +
+            callibrationData[ALPHABET_IDNEX_A].min +
+            callibrationData[ALPHABET_IDNEX_A].max,
+          color: "hsl(225, 70%, 50%)",
         },
       ]);
     }
